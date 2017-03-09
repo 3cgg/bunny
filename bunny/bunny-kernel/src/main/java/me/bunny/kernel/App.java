55d26887ec8c501052159aa65d84330e1502dedd
J <3cgg@163.com>
2017-03-09 15:00:17 +0800

    -

---
+++ bunny/bunny-kernel/src/main/java/me/bunny/kernel/App.java
@@ -0,0 +1,13 @@
+package me.bunny.kernel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
